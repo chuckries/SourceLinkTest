b44e39b41edf94882203aba7989f458066e5fe82
Chuck Ries <Chuck.Ries@microsoft.com>
2016-10-03 12:35:04 -0700

    more

---
+++ PortableNoSourceLink/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortableNoSourceLink
+{
+    public class PortableNoSourceLink
+    {
+        public static void Method()
+        {
+            try
+            {
+                throw new Exception();
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+    }
+}
